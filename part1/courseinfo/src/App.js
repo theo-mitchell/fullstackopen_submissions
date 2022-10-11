191699a50fd15b2c0615842410eb85710ea37c72
theom95 <theodore.mitchell95@gmail.com>
2022-10-10 21:28:23 -0500

    step 1.2 done

--- part1/courseinfo/src/App.js
+++ part1/courseinfo/src/App.js
@@ -6,7 +6,7 @@ const Header = (props) => {
   );
 };
 
-const Content = (props) => {
+const Part = (props) => {
   return (
     <>
       <p>
@@ -16,11 +16,22 @@ const Content = (props) => {
   );
 };
 
+const Content = (props) => {
+  return (
+    <>
+      <Part part={props.part1} exercises={props.exercises1} />
+      <Part part={props.part2} exercises={props.exercises2} />
+      <Part part={props.part3} exercises={props.exercises3} />
+    </>
+  );
+};
+
 const Total = (props) => {
   return (
     <>
       <p>
-        Number of exercises {props.exercises1 + props.exercises2 + props.exercises3}
+        Number of exercises{" "}
+        {props.exercises1 + props.exercises2 + props.exercises3}
       </p>
     </>
   );
@@ -38,9 +49,14 @@ const App = () => {
   return (
     <div>
       <Header course={course} />
-      <Content part={part1} exercises={exercises1} />
-      <Content part={part2} exercises={exercises2} />
-      <Content part={part3} exercises={exercises3} />
+      <Content 
+        part1={part1} 
+        exercises1={exercises1}
+        part2={part2} 
+        exercises2={exercises2} 
+        part3={part3} 
+        exercises3={exercises3}  
+      />
       <Total
         exercises1={exercises1}
         exercises2={exercises2}
