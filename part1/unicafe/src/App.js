50c1115784a60be788c73eda5f3793829ed5aec3
theom95 <theodore.mitchell95@gmail.com>
2022-10-24 21:59:55 -0500

    part 1.8 done

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -1,14 +1,34 @@
 import { useState } from "react";
 
 const Button = (props) => {
+  return <button onClick={props.onClick}>{props.text}</button>;
+};
+
+const FeedbackDisplay = ({ text, quantity }) => {
   return (
-    <button onClick={props.onClick}>{props.text}</button>
+    <p>
+      {text} {quantity}
+    </p>
   );
 };
 
-const FeedbackDisplay = ({text, quantity}) => {
+const Statistics = ({ good, neutral, bad }) => {
   return (
-    <p>{text} {quantity}</p>
+    <>
+      <h1>statistics</h1>
+      <FeedbackDisplay text={"good"} quantity={good} />
+      <FeedbackDisplay text={"neutral"} quantity={neutral} />
+      <FeedbackDisplay text={"bad"} quantity={bad} />
+      <FeedbackDisplay text={"all"} quantity={good + neutral + bad} />
+      <FeedbackDisplay
+        text={"average"}
+        quantity={(good - bad) / (good + bad + neutral) || 0}
+      />
+      <FeedbackDisplay
+        text={"positive"}
+        quantity={((good / (good + bad + neutral)) * 100 || 0) + "%"}
+      />
+    </>
   );
 };
 
@@ -17,34 +37,14 @@ const App = () => {
   const [good, setGood] = useState(0);
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
-  // const [average, setAverage] = useState(0);
-
-  // const setToAverage = () => {
-  //   return setAverage((good - bad) / (good + bad + average));
-  // }
 
   return (
     <>
       <h1>give feedback</h1>
-      <Button 
-        onClick={()=> setGood(good + 1)}
-        text='good'
-      />
-      <Button 
-        onClick={()=> setNeutral(neutral + 1)}
-        text='neutral'
-      />
-      <Button 
-        onClick={()=> setBad(bad + 1)}
-        text='bad'
-      />
-      <h1>statistics</h1>
-      <FeedbackDisplay text={'good'} quantity={good}/>
-      <FeedbackDisplay text={'neutral'} quantity={neutral}/>
-      <FeedbackDisplay text={'bad'} quantity={bad}/>
-      <FeedbackDisplay text={'all'} quantity={good + neutral + bad}/>
-      <FeedbackDisplay text={'average'} quantity={((good - bad) / (good + bad + neutral)) || 0 }/>
-      <FeedbackDisplay text={'positive'} quantity={((good / (good + bad + neutral) * 100) || 0) + '%'}/>
+      <Button onClick={() => setGood(good + 1)} text="good" />
+      <Button onClick={() => setNeutral(neutral + 1)} text="neutral" />
+      <Button onClick={() => setBad(bad + 1)} text="bad" />
+      <Statistics good={good} bad={bad} neutral={neutral} />
     </>
   );
 };
