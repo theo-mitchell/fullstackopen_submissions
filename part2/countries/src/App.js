13692fa455fa21c8150efb4bc2e4d0f796d4f69f
theom95 <theodore.mitchell95@gmail.com>
2022-11-11 16:15:46 -0600

    2.13 done

--- part2/countries/src/App.js
+++ part2/countries/src/App.js
@@ -1,12 +1,13 @@
 import axios from "axios";
 import { useState, useEffect } from "react";
 import Countries from "./components/Countries";
+import Country from "./components/Country";
 import Filter from "./components/Filter";
 
 const App = () => {
   const [countries, setCountries] = useState([]);
   const [displayCountries, setDisplayCountries] = useState([]);
-  const [filter, setFilter] = useState('');
+  const [filter, setFilter] = useState("");
 
   const getCountriesHook = () => {
     axios.get("https://restcountries.com/v3.1/all").then((res) => {
@@ -21,24 +22,48 @@ const App = () => {
 
     setFilter(currentFilter);
     updateDisplayCountries(currentFilter);
-  }
+  };
 
   const updateDisplayCountries = (filter) => {
     let newDisplayCountries = [];
 
-    if(filter != null) {
-      newDisplayCountries = countries.filter((country) => country.name.official.toLowerCase().includes(filter.toLowerCase()));
+    if (filter != null) {
+      newDisplayCountries = countries.filter((country) =>
+        country.name.official.toLowerCase().includes(filter.toLowerCase())
+      );
     } else {
       newDisplayCountries = countries;
     }
-    
+
     setDisplayCountries(newDisplayCountries);
+  };
+
+  const onShowCountry = (country) => {
+    setDisplayCountries([country]);
+    // setFilter('');
   }
+
+  const renderContent = () => {
+    if (displayCountries.length > 10 && filter.length > 0) {
+      return <div>Too many matches, specify another filter</div>;
+    } else if (displayCountries.length === 1) {
+      return <Country country={displayCountries[0]} />;
+    } else if (displayCountries.length === 0) {
+      return <>No countries found</>;
+    } else {
+      return (
+        <Countries 
+          countries={displayCountries}
+          onShowCountry={onShowCountry}
+        />
+      );
+    }
+  };
 
   return (
     <div>
       <Filter value={filter} onChange={handleFilterChange} />
-      <Countries countries={displayCountries} filter={filter}/>
+      {renderContent()}
     </div>
   );
 };
---
+++ part2/countries/src/components/Country.js
@@ -0,0 +1,32 @@
+const Country = ({ country }) => {
+  const getLanguagesListItems = (country) => {
+    const content = [];
+
+    for (const key in country.languages) {
+      content.push(
+        <li key={country.name.official + country.languages[key]}>
+          {country.languages[key]}
+        </li>
+      );
+    }
+
+    if (content.length > 0) {
+      return content;
+    }
+  };
+
+  return (
+    <>
+      <h1>{country.name.official}</h1>
+      <div>Capital: {country.capital}</div>
+      <div>Area: {country.area}</div>
+      <h4>Languages:</h4>
+      <ul>{getLanguagesListItems(country)}</ul>
+      <div>
+        <img src={country.flags.png} alt="flag of selected country"></img>
+      </div>
+    </>
+  );
+};
+
+export default Country;
