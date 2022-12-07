3d11bd63399d81e513ec5ba85f276d8d59bb2772
theom95 <theodore.mitchell95@gmail.com>
2022-12-07 16:00:27 -0600

    4.4 actually done now

--- part4/bloglist/tests/list_helper.test.js
+++ part4/bloglist/tests/list_helper.test.js
@@ -10,11 +10,12 @@ test("dummy returns one", () => {
 describe("total likes", () => {
   const listWithOneBlog = [
     {
-      id: "5a422aa71b54a676234d17f8",
+      __id: "5a422aa71b54a676234d17f8",
       title: "Go To Statement Considered Harmful",
       author: "Edsger W. Dijkstra",
       url: "http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html",
       likes: 5,
+      __v: 0,
     },
   ];
 
@@ -22,5 +23,70 @@ describe("total likes", () => {
     const result = listHelper.totalLikes(listWithOneBlog);
 
     expect(result).toBe(5);
+  });
+
+  const listWithMultipleBlogs = [
+    {
+      _id: "5a422a851b54a676234d17f7",
+      title: "React patterns",
+      author: "Michael Chan",
+      url: "https://reactpatterns.com/",
+      likes: 7,
+      __v: 0,
+    },
+    {
+      _id: "5a422aa71b54a676234d17f8",
+      title: "Go To Statement Considered Harmful",
+      author: "Edsger W. Dijkstra",
+      url: "http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html",
+      likes: 5,
+      __v: 0,
+    },
+    {
+      _id: "5a422b3a1b54a676234d17f9",
+      title: "Canonical string reduction",
+      author: "Edsger W. Dijkstra",
+      url: "http://www.cs.utexas.edu/~EWD/transcriptions/EWD08xx/EWD808.html",
+      likes: 12,
+      __v: 0,
+    },
+    {
+      _id: "5a422b891b54a676234d17fa",
+      title: "First class tests",
+      author: "Robert C. Martin",
+      url: "http://blog.cleancoder.com/uncle-bob/2017/05/05/TestDefinitions.htmll",
+      likes: 10,
+      __v: 0,
+    },
+    {
+      _id: "5a422ba71b54a676234d17fb",
+      title: "TDD harms architecture",
+      author: "Robert C. Martin",
+      url: "http://blog.cleancoder.com/uncle-bob/2017/03/03/TDD-Harms-Architecture.html",
+      likes: 0,
+      __v: 0,
+    },
+    {
+      _id: "5a422bc61b54a676234d17fc",
+      title: "Type wars",
+      author: "Robert C. Martin",
+      url: "http://blog.cleancoder.com/uncle-bob/2016/05/01/TypeWars.html",
+      likes: 2,
+      __v: 0,
+    },
+  ];
+
+  test("when multiple entries are provided the total likes is sum of all likes", () => {
+    const result = listHelper.totalLikes(listWithMultipleBlogs);
+
+    expect(result).toBe(36);
+  });
+
+  const emptyList = [];
+
+  test("when no appropriate data is provided, total likes should be 0", () => {
+    const result = listHelper.totalLikes(emptyList);
+
+    expect(result).toBe(0);
   });
 });
