5fe7a070242f135923dee8f6f482419b487a65d7
theom95 <theodore.mitchell95@gmail.com>
2022-12-07 15:50:59 -0600

    4.4 done

--- part4/bloglist/tests/list_helper.test.js
+++ part4/bloglist/tests/list_helper.test.js
@@ -6,3 +6,21 @@ test("dummy returns one", () => {
   const result = listHelper.dummy(blogs);
   expect(result).toBe(1);
 });
+
+describe("total likes", () => {
+  const listWithOneBlog = [
+    {
+      id: "5a422aa71b54a676234d17f8",
+      title: "Go To Statement Considered Harmful",
+      author: "Edsger W. Dijkstra",
+      url: "http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html",
+      likes: 5,
+    },
+  ];
+
+  test("when list has one blog, likes are equal the likes property of it", () => {
+    const result = listHelper.totalLikes(listWithOneBlog);
+
+    expect(result).toBe(5);
+  });
+});
